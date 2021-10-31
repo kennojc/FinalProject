39c80b5a5f9145026bae5029338695c3b87b4a5c
Kenneth Johnson C <kennojc@gmail.com>
2021-10-31 01:07:20 -0300

    feat: Nested forms OK for the admin

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,18 +1,38 @@
 ActiveAdmin.register Product do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-   permit_params :name, :lowest_price, :image, :description, :stock, :certificate, :artist_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :lowest_price, :image, :description, :stock, :certificate, :artist_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  form product: 'A custom title' do |f|
+    f.inputs 
+    f.inputs "Type" do
+      f.input :types, as: :check_boxes
+    end
+    f.inputs "Auction" do
+      f.has_many :auctions do |a|
+        a.input :start, as: :datepicker,
+                             datepicker_options: {
+                                min_date: Date.today,
+                                max_date: Date.today + 1.year
+                              }
+
+        a.input :end, as: :datepicker,
+                             datepicker_options: {
+                                min_date: Date.today + 1.day,
+                                max_date: Date.today + 1.year
+                              }
+    end
+  end
+    f.actions
+  end
+
+
+   permit_params :name, 
+                 :lowest_price, 
+                 :image, 
+                 :description, 
+                 :stock, 
+                 :certificate, 
+                 :artist_id,
+                 auctions_attributes: [:id, :start_date, :end_date, :round, :_destroy],
+                 types_attributes: [:id, :name, :_destroy] 
 end
+
+
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,5 +4,7 @@ class Product < ApplicationRecord
   has_many :auctions
   has_many :user_products
   has_many :users, through: :user_products
+  accepts_nested_attributes_for :auctions, :types, allow_destroy: true
+
 
 end
